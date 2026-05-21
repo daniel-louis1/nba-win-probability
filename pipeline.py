dec860aa881473c7c7dded8ac5418023824f19df
daniel-louis1 <daniel.lourdu.louis@gmail.com>
2026-05-21 16:21:57 -0400

    fist two features added to pipeline

---
+++ pipeline.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from nba_api.stats.endpoints import leaguegamefinder
+from nba_api.stats.endpoints import playbyplayv3
+
+
+def process_game(game_id):
+
+    #get play by play data
+    pbp = playbyplayv3.PlayByPlayV3(game_id)
+    df = pbp.get_data_frames()[0]
+
+    #1st feature
+    df['score_diff'] = (pd.to_numeric(df['scoreHome']) - pd.to_numeric(df['scoreAway'])).ffill()
+
+    #2nd feature
+    df['seconds_remaining'] = df.apply(lambda row: parse_clock(row['clock'], row['period']), axis = 1)
+
+    return df
+
+
+def parse_clock(clock_str, period):
+    min = clock_str.split("M")[0].split("T")[1]
+    seconds = clock_str.split("M")[1].split("S")[0]
+
+    min = float(min)
+    seconds = float(seconds)
+
+    seconds_left_in_quarter = (min * 60) + seconds
+    seconds_remaining =  (4 - period) * 720 + seconds_left_in_quarter
+    return seconds_remaining
+
+result = process_game("0042500312")
+print(result[['score_diff', 'seconds_remaining']])
+
+
