f056f0618dc35bce79b1f2719470016ca0bf629b
daniel-louis1 <daniel.lourdu.louis@gmail.com>
2026-05-23 13:54:15 -0400

    all 4 features completed

--- pipeline.py
+++ pipeline.py
@@ -15,6 +15,14 @@ def process_game(game_id):
     #2nd feature
     df['seconds_remaining'] = df.apply(lambda row: parse_clock(row['clock'], row['period']), axis = 1)
 
+    #3rd feature
+    df['possession'] = df['location'].map({"h" : 1, "v" : 0}).ffill()
+
+    #4th feature
+    df['home_fouls'] = ((df['actionType'] == 'Foul') & (df['location'] == 'h')).astype(int).cumsum()
+    df['away_fouls'] = ((df['actionType'] == 'Foul') & (df['location'] == 'v')).astype(int).cumsum()
+
+
     return df
 
 
@@ -30,6 +38,6 @@ def parse_clock(clock_str, period):
     return seconds_remaining
 
 result = process_game("0042500312")
-print(result[['score_diff', 'seconds_remaining']])
+print(result[['score_diff', 'seconds_remaining', 'possession', 'home_fouls', 'away_fouls']])
 
 
