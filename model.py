5e2be424ba1c226cfeea30822a5a882dfa95f33c
daniel-louis1 <daniel.lourdu.louis@gmail.com>
2026-05-24 14:41:53 -0400

    Add model architecture and improve data pipeline with resume support

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class WinProbabilityModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(4, 16)
+        self.layer2 = nn.Linear(16, 16)
+        self.layer3 = nn.Linear(16,1)
+        
+
+    def forward(self, x):
+        x = torch.relu(self.layer1(x))
+        x = torch.relu(self.layer2(x))
+        x = torch.sigmoid(self.layer3(x))
